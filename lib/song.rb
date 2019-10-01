0715b39be7515c1fb467e497f95ff20cabd1ca5d
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-01 14:27:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,8 +28,17 @@ class Song
   end
   
   def self.genre_count
-    @@genre_count = @@genres
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+        else
+          genre_count[genre] = 1 
+      end
+    end
+    genre_count
   end
+  
   def self.artist_count
     @@artist_count = @@artists
   end
